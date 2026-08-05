ebac527cc654258fa96504658a380bf6af22aabe
Akashneel Ghosh <37897474+akashneelGhoshDev@users.noreply.github.com>
2026-08-05 17:08:10 +0530

    Update projects.ts
    updated playback ids

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -49,7 +49,7 @@ export const projects: Project[] = [
     brand: "Pigeon",
     description: `The brief was to make it fast-paced and culturally relevant for Gen Z, weaving the product naturally into everyday life. That's exactly the approach we took. To connect with Gen Z, realism matters even more. They're the first to spot AI slop, and that's the last thing we want associated with any brand we work with. Every frame was crafted to feel believable, cinematic, and grounded in reality.`,
     type: "AI Studio",
-    playbackId: "k4YUkKuaQM3E6b6o2wwFNjtJfSjUtadcHMdMebaDeBo",
+    playbackId: "5y65qy3gkWXLBeah4ie6bPBq6qeGMA4F3pFRm6q8KiE",
     category: "ai-studio",
   },
 
@@ -59,7 +59,7 @@ export const projects: Project[] = [
     brand: "1857: The Beginning",
     description: `An empire that ruled. A people who resisted. A revolution that echoed through generations. We wrote this as a feature film, built around courage, sacrifice and unity. Production was about scale with soul. No empty spectacle, no shortcuts. Post was driven by music and pacing. AI was our cinematographer. Even after 169 years, the story of our brave soldiers remains relevant,a reminder that when people stand united against tyranny, a nation can never be broken.`,
     type: "AI Studio",
-    playbackId: "c4dz502ocPm47TiEbj5H43Sg00OIgq6rtZPf1Q5tFaILw",
+    playbackId: "eLlltbJiU6M6YrLo9ihZ9ibWxaZIDD45rl3NfvDg9T8",
     category: "ai-studio",
   },
 
@@ -96,7 +96,7 @@ We obsessed over every detail, the jodhpuri attire in the final frame, the silen
     brand: "Span Floors",
     description: `This project involved complex action sequences where accurate physics was critical. Without it, the visuals would have felt like AI slop. We focused heavily on realism and storytelling. The objective was simple: a family spending quality time in their farmhouse. The home already looked luxurious, but it still felt incomplete. As wood became part of the space, it brought warmth, character, and the soul that truly made the house feel alive.`,
     type: "Cinematic Product Film",
-    playbackId: "x6QfoIORpPgmf815Dg7Ti1C02MPxS017R4UrTW2Rg01fxk",
+    playbackId: "0278wOmRZRLRdxhLiK00XsfcWhHh7vGjCRUTp3czQ6414",
     featured: true,
     category: "work",
   },
